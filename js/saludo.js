bef6207c96a61ad431aa6ee005eadd100a9352a6
mengoaaa <77088042+mengoaaa@users.noreply.github.com>
2021-01-07 12:26:06 -0500

    Add files via upload

---
+++ js/saludo.js
@@ -0,0 +1,8 @@
+<html>
+<title>file:///E:/gaimer/index.html</title>
+<script language="javascript"
+// recoger un dato por teclado y visualizarlo var nom;
+nom=prompt/"escribe tu nombre","Nombre");
+alert ("Mucho gusto"+nom);
+</script>
+</html>
